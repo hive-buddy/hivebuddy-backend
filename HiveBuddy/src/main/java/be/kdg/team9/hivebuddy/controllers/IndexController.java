0242d43b59e26d08ec804ee3aba613ac142107b4
anna.deineko <anna.deineko@student.kdg.be>
2023-11-16 18:48:42 +0100

    index and temperature pages added

---
+++ HiveBuddy/src/main/java/be/kdg/team9/hivebuddy/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package be.kdg.team9.hivebuddy.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping
+    public String getIndexPage(){
+        return "index";
+    }
+}
