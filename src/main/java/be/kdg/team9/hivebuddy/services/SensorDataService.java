e40823ecdf7692479e5d7348a0db2c801e5605e9
anna.deineko <anna.deineko@student.kdg.be>
2023-12-01 13:45:03 +0100

    interface added

--- src/main/java/be/kdg/team9/hivebuddy/services/SensorDataService.java
+++ src/main/java/be/kdg/team9/hivebuddy/services/SensorDataService.java
@@ -1,2 +1,10 @@
-package be.kdg.team9.hivebuddy.services;public interface SensorDataService {
+package be.kdg.team9.hivebuddy.services;
+
+import be.kdg.team9.hivebuddy.data.SensorData;
+
+import java.util.List;
+
+public interface SensorDataService {
+    public List<SensorData> getSensorDatas();
+    public void addNewData(SensorData sensorData);
 }
