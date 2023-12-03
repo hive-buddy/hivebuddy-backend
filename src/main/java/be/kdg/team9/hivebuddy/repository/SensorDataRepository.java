dcca5a4517971afbd64fc963f96fb8b286e16bd3
Ross Rucka <rostislav.rucka@student.kdg.be>
2023-12-03 21:27:04 +0100

    getSensorDataLatestById done

--- src/main/java/be/kdg/team9/hivebuddy/repository/SensorDataRepository.java
+++ src/main/java/be/kdg/team9/hivebuddy/repository/SensorDataRepository.java
@@ -1,14 +1,20 @@
 package be.kdg.team9.hivebuddy.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import be.kdg.team9.hivebuddy.data.SensorData;
 
 @Repository
 public interface SensorDataRepository extends JpaRepository<SensorData,Long> {
-    List<SensorData> findAllLatestByHiveId(Long hive_id);
+//    List<SensorData> findAllLatestByHiveId(Long hive_id);
+    @Query("select a from SensorsData a where a.timestamp <= :creationDateTime and a.hiveId = :hive_id ORDER BY timestamp DESC LIMIT 5")
+    List<SensorData> findAllLatestByHiveId(Long hive_id,
+            @Param("creationDateTime") LocalDateTime creationDateTime);
 }
