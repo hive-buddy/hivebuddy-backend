b78524623bfe456429aa85d0d2da0970182580f0
Oleksii Demydenko <oleksii.demydenko@student.kdg.be>
2023-12-04 01:16:21 +0100

    added getter for retrieving data from DB, added createCSVForSensorData method for creating CSV files

--- src/main/java/be/kdg/team9/hivebuddy/services/SensorDataService.java
+++ src/main/java/be/kdg/team9/hivebuddy/services/SensorDataService.java
@@ -2,9 +2,13 @@ package be.kdg.team9.hivebuddy.services;
 
 import be.kdg.team9.hivebuddy.data.SensorData;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 public interface SensorDataService {
     public List<SensorData> getSensorDatas();
     public void addNewData(SensorData sensorData);
+//    public List<Object[]> getDataFrom(String sensorID, Date startDate);
+    public void createCSVForSensorData(String sensorID, LocalDateTime startDate);
 }
