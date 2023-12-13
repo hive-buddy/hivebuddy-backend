b60f5d22d11f0e30ac0f25404b8c2cfd603c1e7f
Ross Rucka <rostislav.rucka@student.kdg.be>
2023-12-13 10:05:05 +0100

    arduino serial transfered to separate repo

--- src/main/java/be/kdg/team9/hivebuddy/HiveBuddyApplication.java
+++ src/main/java/be/kdg/team9/hivebuddy/HiveBuddyApplication.java
@@ -1,25 +1,7 @@
 package be.kdg.team9.hivebuddy;
 
-//import be.kdg.team9.hivebuddy.data.ArduinoDataReceiver;
-/*
-TODO: remove 4 commented lines below
- */
-
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.List;
-
-import be.kdg.team9.hivebuddy.data.ArduinoDataReceiver;
-import com.fazecast.jSerialComm.SerialPort;
-import java.util.Timer;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import be.kdg.team9.hivebuddy.data.SensorData;
-
 
 @SpringBootApplication
 public class HiveBuddyApplication {
