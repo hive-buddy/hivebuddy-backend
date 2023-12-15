7868c33a09fc6f499b3612123f18de2bcfe12594
Ross Rucka <rostislav.rucka@student.kdg.be>
2023-12-15 07:44:22 +0100

    websocket realtime data showing on website

--- src/main/java/be/kdg/team9/hivebuddy/services/SensorDataService.java
+++ src/main/java/be/kdg/team9/hivebuddy/services/SensorDataService.java
@@ -1,6 +1,8 @@
 package be.kdg.team9.hivebuddy.services;
 
 import be.kdg.team9.hivebuddy.data.SensorData;
+import jakarta.xml.bind.util.JAXBSource;
+import org.json.JSONObject;
 
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -9,7 +11,7 @@ import java.util.List;
 public interface SensorDataService {
     public List<SensorData> getSensorDatas();
     public void addNewData(SensorData sensorData);
-    public void showRealtime(List<SensorData> sensorDataList);
+    public void showRealtime(SensorData sensorData);
 //    public List<Object[]> getDataFrom(String sensorID, Date startDate);
     public void createCSVForSensorData(String sensorID, LocalDateTime startDate);
 }
