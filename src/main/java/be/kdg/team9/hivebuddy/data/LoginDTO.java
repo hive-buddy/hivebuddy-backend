0571b90acaec01716c3eb494847abf6fbe7d3eb7
anna.deineko <anna.deineko@student.kdg.be>
2023-12-23 23:17:22 +0100

    log in with hiveId

---
+++ src/main/java/be/kdg/team9/hivebuddy/data/LoginDTO.java
@@ -0,0 +1,17 @@
+package be.kdg.team9.hivebuddy.data;
+
+public class LoginDTO {
+    private String hiveId;
+
+    public LoginDTO(String hiveId) {
+        this.hiveId = hiveId;
+    }
+
+    public String getHiveId() {
+        return hiveId;
+    }
+
+    public void setHiveId(String hiveId) {
+        this.hiveId = hiveId;
+    }
+}
